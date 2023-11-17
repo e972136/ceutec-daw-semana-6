80c9e006cf562e99e2d596f8ccedf75e0144d467
gasparPizzati <e972136@gmail.com>
2023-11-17 15:57:13 -0600

    Hasta minuto 38:53

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let inputNombre = document.getElementById("nombre");
+let inputEmail = document.getElementById("email");
+let inputEdad = document.getElementById("edad");
+let inputTelefono = document.getElementById("telefono");
+let botonGuardar = document.getElementById("btnGuardar");
+let listaInfo = document.getElementById("listaInfo");
+
+
+console.log(listaInfo);
