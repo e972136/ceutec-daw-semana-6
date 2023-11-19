fe289faf6d1b8da7fbf72edf5abc93fc6b9a63df
gaspar calix <e972136@gmail.com>
2023-11-18 22:03:59 -0600

    Agregar la forma fresa al formulrio

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
-let inputNombre = document.getElementById("nombre");
-let inputEmail = document.getElementById("email");
-let inputEdad = document.getElementById("edad");
-let inputTelefono = document.getElementById("telefono");
 let botonGuardar = document.getElementById("btnGuardar");
 let listaInfo = document.getElementById("listaInfo");
 
 
-console.log(listaInfo);
+function guardarInfo(){
+    listaInfo.innerHTML = "";
+    let inputNombre = document.getElementById("nombre");
+    let inputEmail = document.getElementById("email");
+    let inputEdad = document.getElementById("edad");
+    let inputTelefono = document.getElementById("telefono");
+
+    let html="";
+    html = '<li class="list-group-item" onmouseover=changeColor(this) onmouseout=changeColorOut(this)>'+
+    "Informacion Ingresada<br><br>"+
+    inputNombre.value+"<br>"+
+    inputEmail.value+"<br>"+
+    inputEdad.value+"<br>"+
+    inputTelefono.value+"<br>"+"<hr>"+
+    '</li>';
+//    console.log(listaInfo.innerHTML);
+//    listaInfo.innerHTML += html;
+    listaInfo.innerHTML = html;
+}
+
+function changeColor(element){
+    element.style.backgroundColor="green";
+    element.style.color="white";
+
+}
+
+function changeColorOut(element){
+    element.style.backgroundColor="white";
+    element.style.color="black";
+
+}
+
+
+botonGuardar.addEventListener("click",guardarInfo)
